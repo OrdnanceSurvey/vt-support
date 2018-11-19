d90e601e0bf8267ea88c2e5ec0def9a1ca0b2348
Oliver Snowden <oliversnowden@snodnipper.co.uk>
2018-11-19 12:11:45 +0000

    Add draft attribute aggregate code
    Note: should consider removing serialization / deserialization concern from
    the AttributionClusterer.

---
+++ vt-java/src/main/java/uk/os/vt/cluster/attribution/DoubleSummary.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (C) 2016 Ordnance Survey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.os.vt.cluster.attribution;
+
+import java.util.Objects;
+
+public final class DoubleSummary {
+
+  private final double max;
+  private final double min;
+  private final long samples;
+  private final double sum;
+
+  /**
+   * Merge another entry with this {@link DoubleSummary}.
+   *
+   * @param summary {@link DoubleSummary} to combine
+   * @param value the extra value aggregate
+   */
+  public DoubleSummary(DoubleSummary summary, double value) {
+    Objects.requireNonNull(summary);
+    this.max = Math.max(summary.max, value);
+    this.min = Math.min(summary.min, value);
+    this.samples = summary.samples + 1;
+    this.sum = summary.sum + value;
+  }
+
+  /**
+   * Merge multiple {@link DoubleSummary} items.
+   *
+   * @param vars at least one {@link DoubleSummary}
+   */
+  public DoubleSummary(DoubleSummary... vars) {
+    throwIfEmpty(vars);
+    double max = vars[0].max;
+    double min = vars[0].min;
+    long samples = vars[0].samples;
+    double sum = vars[0].sum;
+
+    for (int i = 1; i < vars.length; i++) {
+      max = Math.max(max, vars[i].max);
+      min = Math.min(min, vars[i].min);
+      samples += vars[i].samples;
+      sum += vars[i].sum;
+    }
+    this.max = max;
+    this.min = min;
+    this.samples = samples;
+    this.sum = sum;
+  }
+
+  /**
+   * Merge multiple doubles.
+   *
+   * @param vars at least one double to aggregate
+   */
+  public DoubleSummary(double... vars) {
+    throwIfEmpty(vars);
+    double max = vars[0];
+    double min = vars[0];
+    long samples = 1;
+    double sum = vars[0];
+
+    for (int i = 1; i < vars.length; i++) {
+      max = Math.max(max, vars[i]);
+      min = Math.min(min, vars[i]);
+      samples++;
+      sum += vars[i];
+    }
+    this.max = max;
+    this.min = min;
+    this.samples = samples;
+    this.sum = sum;
+  }
+
+  protected DoubleSummary(double max, double min, long samples, double sum) {
+    this.max = max;
+    this.min = min;
+    this.samples = samples;
+    this.sum = sum;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    DoubleSummary that = (DoubleSummary) obj;
+    return Double.compare(that.max, max) == 0
+        && Double.compare(that.min, min) == 0
+        && samples == that.samples
+        && Double.compare(that.sum, sum) == 0;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(max, min, samples, sum);
+  }
+
+  public double getMax() {
+    return max;
+  }
+
+  public double getMin() {
+    return min;
+  }
+
+  public double getSum() {
+    return sum;
+  }
+
+  public long getTotalSamples() {
+    return samples;
+  }
+
+  public double getMean() {
+    return sum / samples;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("[max: %f, min: %f, samples: %d, sum: %f]", max, min, samples, sum);
+  }
+
+  private static void throwIfEmpty(double... vars) {
+    if (vars.length == 0) {
+      throw new IllegalArgumentException("nothing to merge!");
+    }
+  }
+
+  private static void throwIfEmpty(DoubleSummary... vars) {
+    if (vars.length == 0) {
+      throw new IllegalArgumentException("nothing to merge!");
+    }
+  }
+}
---
+++ vt-java/src/test/java/uk/os/vt/cluster/attribution/DoubleSummaryTest.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2016 Ordnance Survey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.os.vt.cluster.attribution;
+
+import static org.junit.Assert.assertEquals;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.junit.Test;
+
+public class DoubleSummaryTest {
+
+  private static final double DELTA = 0.00001D;
+
+  @Test
+  public void first() {
+    double max = 1D;
+    double min = 1D;
+    long samples = 2L;
+    double sum = 2D;
+
+    DoubleSummary summary = new DoubleSummary(max, min, samples, sum);
+    DoubleSummary merged = new DoubleSummary(summary, 1L);
+
+    DoubleSummary expected = new DoubleSummary(max, min, samples + 1, sum + 1);
+    checkEquals(expected, merged);
+  }
+
+  @Test
+  public void two() {
+    double min = 10;
+    double max = 55;
+
+    DoubleSummary doubleSummary = new DoubleSummary(min, max);
+
+    assertEquals(min, doubleSummary.getMin(), DELTA);
+    assertEquals(max, doubleSummary.getMax(), DELTA);
+    assertEquals(2, doubleSummary.getTotalSamples(), DELTA);
+    assertEquals((min + max) / 2, doubleSummary.getMean(), DELTA);
+  }
+
+  @Test
+  public void merge() {
+    DoubleSummary doubleSummary = new DoubleSummary(15, 25);
+    DoubleSummary doubleSummary2 = new DoubleSummary(103, 105);
+    DoubleSummary doubleSummary3 = new DoubleSummary(doubleSummary, doubleSummary2);
+
+    assertEquals(105, doubleSummary3.getMax(), DELTA);
+    assertEquals(15, doubleSummary3.getMin(), DELTA);
+    assertEquals(62, doubleSummary3.getMean(), DELTA);
+    assertEquals(248, doubleSummary3.getSum(), DELTA);
+    assertEquals(4, doubleSummary3.getTotalSamples(), DELTA);
+  }
+
+  @Test
+  public void checkToString() {
+    DoubleSummary doubleSummary = new DoubleSummary(15);
+
+    String expected = "[max: 15.000000, min: 15.000000, samples: 1, sum: 15.000000]";
+    String actual = doubleSummary.toString();
+    assertEquals(expected, actual);
+
+    DoubleSummary doubleSummary2 = new DoubleSummary(15, 25);
+
+    expected = "[max: 25.000000, min: 15.000000, samples: 2, sum: 40.000000]";
+    actual = doubleSummary2.toString();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void checkEquality() {
+    EqualsVerifier.forClass(DoubleSummary.class)
+        .suppress(Warning.NONFINAL_FIELDS)
+        .verify();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void checkEmpty() {
+    new DoubleSummary(new DoubleSummary[]{});
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void checkEmpty2() {
+    new DoubleSummary((new double[]{}));
+  }
+
+  private void checkEquals(DoubleSummary one, DoubleSummary two) {
+    assertEquals(one.getMax(), two.getMax(), DELTA);
+    assertEquals(one.getMin(), two.getMin(), DELTA);
+    assertEquals(one.getTotalSamples(), two.getTotalSamples());
+    assertEquals(one.getSum(), two.getSum(), DELTA);
+  }
+}
